f927ff93ff9435bf22d20273d060fe232536d7ef
Joe Glass-Katz <jsg1@Joes-MacBook-Air.local>
2018-09-14 16:37:07 -0700

    Add lemma merge

---
+++ merge_lemma.py
@@ -0,0 +1,9 @@
+'''
+
+This file is to merge and clean vegetation datasets.
+It merges plant traits to species found in the pacific
+northwest. Several datasets have been combined to create
+this body of information. Citations for the data can be found in
+the notes file.
+
+'''
